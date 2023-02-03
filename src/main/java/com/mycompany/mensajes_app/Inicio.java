da0e5032e938e405d9496b59e8d074b249cf5623
Stiven Ramos <StivenRMS>
2023-02-02 23:06:50 -0600

    Conexion y Create Listo!

--- src/main/java/com/mycompany/mensajes_app/Inicio.java
+++ src/main/java/com/mycompany/mensajes_app/Inicio.java
@@ -5,21 +5,47 @@
 package com.mycompany.mensajes_app;
 
 import java.sql.Connection;
+import java.util.Scanner;
 
 /**
  *
  * @author Usuario
  */
 public class Inicio {
+
     public static void main(String[] args) {
-        
-        Conexion conexion = new Conexion();
-        try (Connection cnx = conexion.get_Connection()) {
-            
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+        do {
+            System.out.println("----------------");
+            System.out.println(" Aplicacion de mensajes");
+            System.out.println("1. crear mensajes");
+            System.out.println("2. listar mensajes");
+            System.out.println("3. editar mensajes");
+            System.out.println("4. elimnar mensajes");
+            System.out.println("5. salir");
+            //leemos la opcion del usuario
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    mensajesService.crearMensaje();
+                    break;
+                case 2:
+                    mensajesService.listarMensajes();
+                    break;
+                case 3:
+                    mensajesService.borrarMensajes();
+                    break;
+                case 4:
+                    mensajesService.editarMensajes();
+                    break;
+                default:
+                    break;
+            }
+
+        } while (opcion != 5);
+
     }
-    
+
 }
